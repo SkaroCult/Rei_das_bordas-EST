2834644fdd84e38c88c38ee2151778dcc643151e
SkaroCult <supernetroy@gmail.com>
2023-10-13 11:09:06 -0300

    0.03 - Adicionando paginas

--- index.js
+++ index.js
@@ -1,4 +1,17 @@
+
 function openAba(id){
-    const mensagem = `Nome ${id}`
-    console.log(mensagem)
+    let body = document.getElementsByTagName('body')
+    const pag = document.getElementById(id)
+    console.log(pag)
+
+    window.scrollTo({top:0, behavior:"instant"})
+    document.body.style.overflow = 'hidden'
+    pag.style.display = 'flex'
+}
+
+function fechar(id){
+    let pag = document.getElementById(id)
+
+    pag.style.display = 'none'
+    document.body.style.overflowY = 'scroll'
 }
